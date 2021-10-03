e63b4deca47e5002ef156c8c60a51adb24283588
apudebnath <apu1578@gmail.com>
2021-10-04 03:03:16 +0600

    Data loaded

---
+++ jsonData.js
@@ -0,0 +1,98 @@
+[
+    {
+      "id": 201012,
+      "title": "Basic to advanced HTML and CSS",
+      "picture": "https://i.ibb.co/TgNh8pj/htmlandcss.jpg",
+      "price": 69,
+      "language": "English",
+      "description": "HTML (Hypertext Markup Language) is the code that is used to structure a web page and its content. CSS stands for Cascading Style Sheets. It is the language for describing the presentation of Web pages, including colours, layout, and fonts, thus making our web pages presentable to the users. ... "
+    },
+    {
+      "id": 202022,
+      "title": "Basic JavaScript",
+      "picture": "https://i.ibb.co/TL0gRhD/javascript.jpg",
+      "price": 59,
+      "language": "English",
+      "description": "JavaScript is a programming language that adds interactivity to your website. This happens in games, in the behavior of responses when buttons are pressed or with data entry on forms; with dynamic styling; with animation, etc"
+    },
+    {
+      "id": 203032,
+      "title": "Advanced JavaScript",
+      "picture": "https://i.ibb.co/4tdr7sz/javascript1.jpg",
+      "price": 159,
+      "language": "English",
+      "description": "JavaScript is a programming language that adds interactivity to your website. This happens in games, in the behavior of responses when buttons are pressed or with data entry on forms; with dynamic styling; with animation, etc"
+    },
+    {
+      "id": 204042,
+      "title": "Basic PHP",
+      "picture": "https://i.ibb.co/jL0x5fs/php.jpg",
+      "price": 59,
+      "language": "English",
+      "description": "PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP."
+    },
+    {
+      "id": 205052,
+      "title": "Advanced PHP",
+      "picture": "https://i.ibb.co/jvTSZwR/php1.jpg",
+      "price": 159,
+      "language": "English",
+      "description": "PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP."
+    },
+    {
+      "id": 206062,
+      "title": "Advanced Python",
+      "picture": "https://i.ibb.co/0hJFRqR/python.jpg",
+      "price": 159,
+      "language": "English",
+      "description": "Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis. Python is a general purpose language, meaning it can be used to create a variety of different programs and isn't specialized for any specific problems"
+    },
+    {
+      "id": 207072,
+      "title": "Basic to advanced C++",
+      "picture": "https://i.ibb.co/z262cb1/cplus.jpg",
+      "price": 159,
+      "language": "English",
+      "description": "C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or 'C with Classes'."
+    },
+    {
+      "id": 208082,
+      "title": "Basic to advanced Ruby",
+      "picture": "https://i.ibb.co/WHcvwJR/ruby.jpg",
+      "price": 259,
+      "language": "English",
+      "description": "Ruby is an interpreted, high-level, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro 'Matz' Matsumoto in Japan. Ruby is dynamically typed and uses garbage collection and just-in-time compilation."
+    },
+    {
+      "id": 209092,
+      "title": "Machine Learning",
+      "picture": "https://i.ibb.co/K7FBqjL/mechine-Learning.jpg",
+      "price": 259,
+      "language": "English",
+      "description": "Machine learning is an application of artificial intelligence (AI) that provides systems the ability to automatically learn and improve from experience without being explicitly programmed. Machine learning focuses on the development of computer programs that can access data and use it to learn for themselves"
+    },
+    {
+      "id": 209992,
+      "title": "Data Science",
+      "picture": "https://i.ibb.co/SXL3nCp/datascience1.jpg",
+      "price": 159,
+      "language": "English",
+      "description": "Machine learning is an application of artificial intelligence (AI) that provides systems the ability to automatically learn and improve from experience without being explicitly programmed. Machine learning focuses on the development of computer programs that can access data and use it to learn for themselves"
+    },
+    {
+      "id": 209993,
+      "title": "Advanced Data Science",
+      "picture": "https://i.ibb.co/y04Lj8t/datascience.jpg",
+      "price": 259,
+      "language": "English",
+      "description": "Machine learning is an application of artificial intelligence (AI) that provides systems the ability to automatically learn and improve from experience without being explicitly programmed. Machine learning focuses on the development of computer programs that can access data and use it to learn for themselves"
+    },
+    {
+      "id": 209995,
+      "title": "Web Development",
+      "picture": "https://i.ibb.co/wyC1NF9/webdevelopment.jpg",
+      "price": 259,
+      "language": "English",
+      "description": "Web development is the work involved in developing a Web site for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services"
+    }
+  ]
